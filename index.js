9e85a68e0fb891b8c264b2fc6681b36420ffa577
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-06 14:20:57 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arrayOfDrivers) => {
+  const drivers = [...arrayOfDrivers].splice(0,2)
+  return drivers
+}
+
+const returnLastTwoDrivers = (arrayOfDrivers) => {
+  const drivers = [...arrayOfDrivers].splice(-2,2)
+  return drivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+let createFareMultiplier = fare => {
+  return multiplier => fare * multiplier
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (array, fn) => {
+  return fn(array)
+}
